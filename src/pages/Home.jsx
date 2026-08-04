23e95103ba0cab2d68cedadd0ea8eb979f6acc8b
Maxdeoro <maxdeoro@gmail.com>
2026-08-04 23:22:33 +0300

    create page links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
             HOME
+            <Link to='/'>Home</Link>
+            <Link to='about'>About</Link>
+            <Link to='categories'>Categories</Link>
+            <Link to='product'>Product details</Link>
+            <Link to='cart'>Cart</Link>
         </div>
     )
 }
 
-export default Home
+export default Home;
 
