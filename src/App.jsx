86288f4d3335790ec645ac22b1093af65b6b1791
Maxdeoro <maxdeoro@gmail.com>
2026-08-08 00:42:17 +0300

    implement component Layout/Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
 import Categories from "./pages/Categories";
 import ProductDetails from "./pages/ProductDetails";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
-const router = createBrowserRouter([{path: '/', element: <><Header/><Home/><Footer/></>},
-                                    {path: 'about', element: <><Header/><About/><Footer/></>},
-                                    {path: 'cart', element: <><Header/><Cart/><Footer/></>},
-                                    {path: 'categories', element: <><Header/><Categories/><Footer/></>},
-                                    {path: '*', element: <><Header/><NotFound/><Footer/></>},
-                                    {path: 'product', element: <><Header/><ProductDetails/><Footer/></>},
-]);
+const router = createBrowserRouter([{path: '/', element: <Layout />, children: [
+  {path: '', element: <Home />},
+  {path: 'about', element: <About />},
+  {path: 'cart', element: <Cart />},
+  {path: 'categories', element: <Categories />},
+  {path: '*', element: <NotFound />},
+  {path: 'product', element: <ProductDetails />},
+]}]);
 
 function App() {
   return <RouterProvider router={router}/>;
