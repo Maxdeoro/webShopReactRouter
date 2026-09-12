4d6ee2f30cbab60aa6062ac9e3332f39fd9a7fef
Maxdeoro <maxdeoro@gmail.com>
2026-09-12 23:36:32 +0300

    height,padding,margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,28 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
-        <header className="bg-lime-200">
+        <header className="bg-lime-200 py-8 px-5">
+            <img className="h-6" src="/logo.svg" alt="logo" />
             <h1 className="text-3xl text-text">Header</h1>
-            <NavLink to='/' className='font-bold'>Home</NavLink>
-            <NavLink to='/about' className='font-bold'>About</NavLink>
-            <NavLink to='/categories' className='font-bold'>Categories</NavLink>
-            <NavLink to='/product' className='font-bold'>Product details</NavLink>
-            <NavLink to='/cart' className='font-bold'>Cart</NavLink>
-            <hr></hr>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to='/' className='font-bold'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about' className='font-bold'>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/cart' className='font-bold'>Cart</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/categories' className='font-bold'>Categories</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/product' className='font-bold'>Product details</NavLink>
+                    </li>
+                </ul>    
+            </nav>
         </header>
     )
 };
