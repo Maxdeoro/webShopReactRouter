3ff55a372bb3b6047d0e593ae2bc9f79e27429c4
Maxdeoro <maxdeoro@gmail.com>
2026-09-13 21:35:11 +0300

    grid in tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
+        <div className='py-10'>
             <Link to='/category/Electronics' state={{from: 'Home page', maxPrice: 600}}>
                  Electronics at low price
             </Link>
-            {/* <h1>Categories</h1> */}
-            <ul className='flex gap-x-5 justify-around'>
+            <h1 className='text-2xl font-semibold text-center mb-8'>Categories</h1>
+            <ul className='grid grid-cols-3 gap-4 px-5'>
             {/* <ul style={{display: 'flex'}} className='flex gap-x-5'> */}
                 {categories.map(category => (
                     <li key={category.id}>
                         <Link to={`/category/${category.name}`}>
                             {category.name}
-                            <img src={category.img} alt={category.name} style={{width: '150px'}}/>
+                            <img src={category.img} alt={category.name} />
                         </Link>
                     </li>
                 ))}
