8d643fb110f9e7d81016191fc75e6bad831f43ff
Maxdeoro <maxdeoro@gmail.com>
2026-09-19 22:43:30 +0300

    NavLink state

--- src/components/ui/NavLinkMenu.jsx
+++ src/components/ui/NavLinkMenu.jsx
@@ -2,7 +2,12 @@ import { NavLink } from "react-router-dom";
 
 function NavLinkMenu({to, children}) {
     return (
-         <NavLink to={to} className='text-xl text-gray-500 hover:text-gray-900'>{children}</NavLink>
+        <NavLink to={to} className={({isActive}) => 
+            isActive ? 'text-xl text-gray-900 font-semibold hover:text-blue-600' 
+            : 'text-xl text-gray-500 hover:text-blue-600 hover:text-2xl'
+        }>
+            {children}
+        </NavLink>
     )
 }
 
